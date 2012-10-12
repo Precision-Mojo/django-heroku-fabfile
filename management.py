fcd898dc11fb752c9453a13fe38867b393d3313e
Marcus R. Brown <mrbrown@precision-mojo.com>
2012-10-11 19:31:59 -0700

    Add the management module and a task to update vendor submodules.

---
+++ management.py
@@ -0,0 +1,50 @@
+"""Tasks that deal with project management."""
+
+import os
+from fabric.api import lcd, local, settings, task
+from django.utils.importlib import import_module
+from django.utils.module_loading import module_has_submodule
+
+from settings import django_settings
+from utils import msg
+
+vendor_modules = None
+
+
+@task
+def update_vendors():
+    """Update vendor modules."""
+    for module in get_vendor_modules():
+        submodules = getattr(module, 'submodules')
+        if submodules is not None:
+            vendor_root = os.path.dirname(module.__file__)
+            for name, params in submodules.iteritems():
+                vendor_path = os.path.join(vendor_root, name)
+                if os.path.isdir(vendor_path):
+                    tag = params.get('tag', '')
+                    with lcd(vendor_path), msg("Updating vendor submodule %s" % name):
+                        local('git checkout %s' % tag)
+                        sources = params.get('sources', [])
+                        destination = params.get('destination')
+                        if destination is not None:
+                            destination = \
+                                os.path.normpath(os.path.join(vendor_root, destination))
+                            with settings(warn_only=True):
+                                local('mkdir -p %s' % destination)
+                            for source in sources:
+                                local('cp -R %s %s' % (source, destination))
+
+
+def get_vendor_modules():
+    global vendor_modules
+    if not vendor_modules:
+        vendor_modules = []
+        for app in django_settings.INSTALLED_APPS:
+            module = import_module(app)
+            try:
+                vendor_module = import_module('%s.vendor' % app)
+                vendor_modules.append(vendor_module)
+            except:
+                if module_has_submodule(module, 'vendor'):
+                    raise
+    return vendor_modules
